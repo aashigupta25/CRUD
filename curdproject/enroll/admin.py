752a500eaffd5be335caf3655d28617ac23f2b4e
Aashi <aashi251198@gmail.com>
2022-10-10 15:11:26 +0530

    Initial commit

---
+++ curdproject/enroll/admin.py
@@ -0,0 +1,8 @@
+from msilib.schema import Class
+from django.contrib import admin
+from .models import User
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'password')
